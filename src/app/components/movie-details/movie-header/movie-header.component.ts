e7b075830889c93b523d77b6b825b90fb665d7de
Kamok1 <kkamiss12@gmail.com>
2023-04-02 18:19:53 +0200

    Add ability to edit favorite movies list

--- src/app/components/movie-details/movie-header/movie-header.component.ts
+++ src/app/components/movie-details/movie-header/movie-header.component.ts
@@ -2,42 +2,58 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from '../../../models/movie';
 import { ApiService } from '../../../services/api.service';
 import { KeyValueDiffer, KeyValueDiffers } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
 
+const filledStarSrc : string = "../../../assets/filledStar.svg"
+const unfilledStarSrc : string = "../../../assets/Star.svg"
+let isViewUpdated : boolean = false;
 @Component({
   selector: 'app-movie-header',
   templateUrl: './movie-header.component.html',
   styleUrls: ['./movie-header.component.css']
 })
 export class MovieHeaderComponent implements OnInit {
-  @Input() movie? : Movie
-  @Input() movieId? : number
-  Movie: Movie = new Movie()
+  @Input() movie : Movie = new Movie()
+
   movieDiffer: KeyValueDiffer<string,any> | undefined
-  NumberOfReviews : number = 0
-  IsFavorite : boolean = false;
-  StarSrc: string =  "../../../assets/Star.svg"
+  numberOfReviews : number = 0
+  isFavorite : boolean = false;
+  starSrc: string = unfilledStarSrc
+  isLogged : boolean = false
+
   constructor(
-    private api: ApiService,
-    private differs: KeyValueDiffers,
+    private _api: ApiService,
+    private _differs: KeyValueDiffers,
   ) {}
 
   ngOnInit(): void {
-    //if(logged)
-    if(this.movie === undefined && this.movieId !== undefined){
-      this.api.GetMovie(this.movieId).subscribe(movie => this.Movie = new Movie(movie))
-      this.api.CountMovieReviews(this.movieId).subscribe(numberOfReviews => this.NumberOfReviews = numberOfReviews)
+    if(isViewUpdated == false){
+      this.movieDiffer = this._differs.find(this.movie).create()
     }
-    else if(this.Movie.title === ""){
-      this.Movie = this.movie!
-      this.movieDiffer = this.differs.find(this.Movie).create()
+    if(this.movie.id !== 0){
+      this._api.CountMovieReviews(this.movie.id).subscribe(numberOfReviews => this.numberOfReviews = numberOfReviews)
+      if(this.isLogged){
+        this._api.isInUserFavoritesMovies(this.movie.id).subscribe(res => {
+          this.isFavorite = res
+          this.updateStar()
+        } )
+      }
     }
   }
-  editFavoriteMovie(){
-    this.StarSrc =  this.StarSrc.includes("filled") ? this.StarSrc.replace("filled","") : this.StarSrc.replace("Star","filledStar")
+  updateStar(){
+    this.starSrc =  this.isFavorite ? filledStarSrc : unfilledStarSrc
+  }
+  async editFavoriteMovie(){
+    let isSuccessful = this.isFavorite ? await this._api.RemoveFromUserFavortiesMovies(this.movie.id).toPromise() : await this._api.addToUserFavortiesMovies(this.movie.id).toPromise()
+    if(!isSuccessful){
+      alert("editing error")
+      return
+    }
+    this.isFavorite = !this.isFavorite
+    this.updateStar()
   }
   ngDoCheck(): void {
-    if(this.movieDiffer?.diff(this.Movie)){
+    if(this.movieDiffer?.diff(this.movie) && !isViewUpdated){
+      isViewUpdated = true;
       setTimeout(()=>{
         this.ngOnInit()
       }, 100)
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Movie } from '../models/movie';
-import { TokenType } from '@angular/compiler';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, catchError, map, of, throwError } from 'rxjs';
 import {AppSettings} from "../../settings/appsettings";
-import { IMovie } from '../interfaces/imovie';
 import { MovieResponse } from '../models/movie-response';
 
-
-const httpOptions = {
+  const httpOptions = {
   headers : new HttpHeaders({
     'Content-Type' : "Appliaction-json"
   })
@@ -24,23 +20,43 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  // GetUserMovie(id : number): Observable<boolean>{
-  //   return this.http.get(AppSettings.API_URL + `/me/movies/check/${id}`)//todo
-  // }
-
+  isInUserFavoritesMovies(movieId : number): Observable<boolean>{
+    return this.http.get<boolean>(AppSettings.API_URL + `/User/me/movies/check/${movieId}`)
+  }
+  addToUserFavortiesMovies(movieId : number) : Observable<boolean> {
+    return this.http.put<any>(AppSettings.API_URL + `/User/me/movies/${movieId}`, {}).pipe(
+      map(res => res.status === 200),
+      catchError(this.handleError)
+    )
+  }
+  RemoveFromUserFavortiesMovies(movieId : number) : Observable<boolean> {
+    return this.http.delete<any>(AppSettings.API_URL + `/User/me/movies/${movieId}`, {}).pipe(
+      map(res => res.status === 200),
+      catchError(this.handleError)
+    )
+  }
   GetMovie(id : number): Observable<MovieResponse>{
-    let params = new HttpParams().set('id', id);
+    let params = new HttpParams().set('id', id)
     return this.http.get<MovieResponse>(AppSettings.API_URL + '/Movie', {params: params})
   }
 
   GetMovies(year? : number, title? : string, genreId? : number, directorId? : number, actorId? : number): Observable<MovieResponse[]> {
-    let params = new HttpParams();
+    let params = new HttpParams()
     if(title !== undefined){
-      params.append("title", title);
+      params.append("title", title)
     }
-    return this.http.get<MovieResponse[]>(AppSettings.API_URL + '/Movie', {params: params});
+    return this.http.get<MovieResponse[]>(AppSettings.API_URL + '/Movie', {params: params})
   }
   CountMovieReviews(id : number): Observable<number>{
     return this.http.get<number>(AppSettings.API_URL + `/Review/${id}/Count`)
   }
+
+  private handleError(error: HttpErrorResponse): Observable<boolean> {
+    if (error.status === 404) {
+      return of(false); // resource not found
+    } else {
+      return throwError(error); // rethrow the error
+    }
+  }
+
 }
