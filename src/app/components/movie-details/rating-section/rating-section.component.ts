c98f6acf4fcb162c7580bb492db0ad9ee78c7b24
Kamok1 <kkamiss12@gmail.com>
2023-05-06 21:42:42 +0200

    add rating seciton

---
+++ src/app/components/movie-details/rating-section/rating-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rating-section',
+  templateUrl: './rating-section.component.html',
+  styleUrls: ['./rating-section.component.scss']
+})
+export class RatingSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
